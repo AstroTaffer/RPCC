dafaa2d269c59353d040a8f53bc0dde84b255223
RoboPhot_win <nikita.googlov@gmail.com>
2023-09-13 18:59:58 +0500

    Camera rework: pit stop 1

--- RPCC/Cams/CameraControl.cs
+++ RPCC/Cams/CameraControl.cs
@@ -3,6 +3,7 @@ using RPCC.Utils;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Timers;
 
 namespace RPCC.Cams
@@ -53,17 +54,72 @@ namespace RPCC.Cams
                     continue;
                 }
 
+                var camSn = new StringBuilder(128);
+                var len = new IntPtr(128);
+                errorLastFliCmd = NativeMethods.FLIGetSerialString(cams[i].handle, camSn, len);
+                if (errorLastFliCmd != 0)
+                {
+                    Logger.AddLogEntry($"WARNING Unable to get camera {i + 1} serial number");
+                    cams[i].serialNumber = "ERROR";
+                }
+                else cams[i].serialNumber = camSn.ToString();
 
+                if (cams[i].serialNumber == Settings.SnCamG) cams[i].filter = "g";
+                else if (cams[i].serialNumber == Settings.SnCamR) cams[i].filter = "r";
+                else if (cams[i].serialNumber == Settings.SnCamI) cams[i].filter = "i";
+                else
+                {
+                    Logger.AddLogEntry($"WARNING Unable to identify camera {i + 1} filter");
+                    cams[i].filter = "UNKNOWN";
+                }
+
+                Logger.AddLogEntry($"Camera {i + 1}: Handle {cams[i].handle} | " +
+                    $"Filename {cams[i].fileName} | " +
+                    $"Model {cams[i].modelName} | Serial Number {cams[i].serialNumber} | " +
+                    $"Filter {cams[i].filter}");
+
+                // unchecked((int)0xffffffff) = FLI_FAN_SPEED_ON
+                errorLastFliCmd = NativeMethods.FLISetFanSpeed(cams[i].handle, unchecked((int)0xffffffff));
+                if (errorLastFliCmd != 0) Logger.AddLogEntry($"WARNING Unable to turn on camera {i + 1} fan");
+
+                // 1 = FLI_MODE_16BIT
+                errorLastFliCmd = NativeMethods.FLISetBitDepth(cams[i].handle, 1);
+                if (errorLastFliCmd != 0) Logger.AddLogEntry($"WARNING Unable to set camera {i + 1} bit depth");
+
+                // 0x0001 = FLI_BGFLUSH_START
+                errorLastFliCmd = NativeMethods.FLIControlBackgroundFlush(cams[i].handle, 0x0001);
+                if (errorLastFliCmd != 0) Logger.AddLogEntry($"WARNING Unable to turn on camera {i + 1} background flush");
+
+                errorLastFliCmd = NativeMethods.FLISetNFlushes(cams[i].handle, Settings.NumFlushes);
+                if (errorLastFliCmd != 0) Logger.AddLogEntry($"WARNING Unable to set camera {i + 1} number of flushes");
+
+                errorLastFliCmd = NativeMethods.FLISetTemperature(cams[i].handle, Settings.CamTemp);
+                if (errorLastFliCmd != 0) Logger.AddLogEntry($"WARNING Unable to set camera {i + 1} temperature");
+
+                // TODO: SetVBin, SetHBin, SetImageArea !
+
+                switch (Settings.CamRoMode)
+                {
+                    // 0 = 2.0 MHz
+                    case "2.0 MHz":
+                        errorLastFliCmd = NativeMethods.FLISetCameraMode(cams[i].handle, 0);
+                        break;
+                    // 1 = 500KHz
+                    case "500KHz":
+                        errorLastFliCmd = NativeMethods.FLISetCameraMode(cams[i].handle, 1);
+                        break;
+                }
+                if (errorLastFliCmd != 0) Logger.AddLogEntry($"WARNING Unable to set camera {i + 1} readout mode");
             }
 
-            camsTimer.Elapsed += CallCameras;
+            camsTimer.Elapsed += CamsTimerTick;
             camsTimer.Start();
         }
 
         static internal void DisconnectCameras()
         {
             camsTimer.Stop();
-            camsTimer.Elapsed -= CallCameras;
+            camsTimer.Elapsed -= CamsTimerTick;
 
             int errorLastFliCmd;
             for (int i = 0; i < cams.Length; i++)
@@ -81,10 +137,59 @@ namespace RPCC.Cams
         }
         #endregion
 
-        #region Timer
-        static private void CallCameras(object sender, ElapsedEventArgs e)
+        #region Status
+        static private void CamsTimerTick(object sender, ElapsedEventArgs e)
         {
 
+        }
+
+        static internal void GetCamsStatus()
+        {
+            int errorStatus;
+
+            foreach (var cam in cams)
+            {
+                errorStatus = 0;
+                cam.remTime = 0;
+
+                // 0x0000 = FLI_TEMPERATURE_CCD
+                errorStatus = NativeMethods.FLIReadTemperature(cam.handle, 0x0000, out cam.ccdTemp);
+
+                // 0x0001 = FLI_TEMPERATURE_BASE
+                errorStatus += NativeMethods.FLIReadTemperature(cam.handle, 0x0001, out cam.baseTemp);
+
+                errorStatus += NativeMethods.FLIGetCoolerPower(cam.handle, out cam.coolerPwr);
+
+                errorStatus += NativeMethods.FLIGetDeviceStatus(cam.handle, out int deviceStatus);
+
+                // I don't know why it's done this way. Someday I'll run corresponding tests and find out. Someday.
+                deviceStatus &= 0x03;
+                switch (deviceStatus)
+                {
+                    // 0x00 = FLI_CAMERA_STATUS_IDLE
+                    case 0x00:
+                        cam.status = "IDLE";
+                        break;
+                    // 0x01 = FLI_CAMERA_STATUS_WAITING_FOR_TRIGGER
+                    case 0x01:
+                        cam.status = "WF TRIGGER";
+                        break;
+                    // 0x02 = FLI_CAMERA_STATUS_EXPOSING
+                    case 0x02:
+                        cam.status = "EXPOSING";
+                        errorStatus += NativeMethods.FLIGetExposureStatus(cam.handle, out cam.remTime);
+                        break;
+                    // 0x03 = FLI_CAMERA_STATUS_READING_CCD
+                    case 0x03:
+                        cam.status = "READING CCD";
+                        break;
+                }
+
+                if (errorStatus != 0)
+                {
+                    cam.status = "ERROR";
+                }
+            }
         }
         #endregion
 
@@ -178,5 +283,6 @@ namespace RPCC.Cams
         // exposure
         internal bool isExposing;
         internal DateTime expStartDt;
+        internal double expStartJd;
     }
 }
