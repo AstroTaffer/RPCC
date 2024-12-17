3bf144c49b1b0f79c87f5451469e1d43a6ae816d
NabatFasetochnii <nikita.googlov@gmail.com>
2024-12-17 17:26:31 +0500

    много мелких изменений

--- RPCC/Tasks/Tasker.cs
+++ RPCC/Tasks/Tasker.cs
@@ -19,7 +19,7 @@ namespace RPCC.Tasks
             "N", "RaDecJ2000", "t_{Added}", @"t_{Run}", @"t_{Fin}", "Duration (h)",
             "Exp", "Done", "All", "t_{Last exp}", "Filter g", "Filter r", "Filter i", 
             "Object", "Object type", "Status", "Observer", "Frame type",
-            "Xbin", "Ybin"
+            "Xbin", "Ybin", "repoint_coords", "repoint_times", "Filter V"
         };
 //gri
 
@@ -47,9 +47,9 @@ namespace RPCC.Tasks
             DataGridViewTasker.Columns[7].Width = 60;
             DataGridViewTasker.Columns[8].Width = 60;
             DataGridViewTasker.Columns[9].Width = 120;
-            DataGridViewTasker.Columns[10].Width = 60;
-            DataGridViewTasker.Columns[11].Width = 60;
-            DataGridViewTasker.Columns[12].Width = 60;
+            DataGridViewTasker.Columns[10].Width = 40;
+            DataGridViewTasker.Columns[11].Width = 40;
+            DataGridViewTasker.Columns[12].Width = 40;
             DataGridViewTasker.Columns[13].Width = 120;
             DataGridViewTasker.Columns[14].Width = 120;
             DataGridViewTasker.Columns[15].Width = 60;
@@ -57,6 +57,9 @@ namespace RPCC.Tasks
             DataGridViewTasker.Columns[17].Width = 80;
             DataGridViewTasker.Columns[18].Width = 40;
             DataGridViewTasker.Columns[19].Width = 40;
+            DataGridViewTasker.Columns[20].Width = 60;
+            DataGridViewTasker.Columns[21].Width = 60;
+            DataGridViewTasker.Columns[22].Width = 40;
 
             foreach (DataGridViewColumn column in DataGridViewTasker.Columns)
                 column.SortMode = DataGridViewColumnSortMode.NotSortable;
@@ -64,41 +67,7 @@ namespace RPCC.Tasks
             // dataGridViewTasker.Sort(dataGridViewTasker.Columns[0], ListSortDirection.Descending);
             // dataGridViewTasker.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.TopCenter;
         }
-
-        // public static void SaveTasksToXml()
-        // {
-        //     var dataTable = (DataTable) dataGridViewTasker.DataSource;
-        //     try
-        //     {
-        //         dataTable.WriteXml(FileName);
-        //     }
-        //     catch (Exception exception)
-        //     {
-        //         Logger.AddLogEntry(exception.Message);
-        //     }
-        // }
-
-        // public static void LoadTasksFromXml()
-        // {
-        //     if (File.Exists(FileName))
-        //     {
-        //         try
-        //         {
-        //             DataSet.ReadXml(FileName);
-        //             dataGridViewTasker.DataSource = DataSet.Tables[0];
-        //             PaintTable();
-        //         }
-        //         catch (XmlException e)
-        //         {
-        //             Logger.AddLogEntry($"Tasker Error: {e.Message}");
-        //         }
-        //     }
-        //     else
-        //     {
-        //         File.Create(FileName);
-        //         SaveTasksToXml();
-        //     }
-        // }
+        
 
 
         public static void PaintTable()
@@ -126,166 +95,10 @@ namespace RPCC.Tasks
                 };
         }
 
-        // public static int GetTasksLen()
-        // {
-        //     return DataGridViewTasker.Rows.Count;
-        // }
-
-        // public static void AddTask(ObservationTask task)
-        // {
-        //     var dataRow = DataTable.NewRow();
-        //     dataRow[Header[0]] = task.TaskNumber;
-        //     dataRow[Header[1]] = task.RaDec;
-        //     dataRow[Header[2]] = task.TimeAdd;
-        //     dataRow[Header[3]] = task.TimeStart;
-        //     dataRow[Header[4]] = task.TimeEnd;
-        //     dataRow[Header[5]] = task.Duration;
-        //     dataRow[Header[6]] = task.Exp;
-        //     dataRow[Header[7]] = task.DoneFrames;
-        //     dataRow[Header[8]] = task.AllFrames;
-        //     dataRow[Header[9]] = task.TimeLastExp;
-        //     dataRow[Header[10]] = task.Filters.Contains("g");
-        //     dataRow[Header[11]] = task.Filters.Contains("r");
-        //     dataRow[Header[12]] = task.Filters.Contains("i");
-        //     dataRow[Header[13]] = task.Object;
-        //     dataRow[Header[14]] = task.ObjectType;
-        //     dataRow[Header[15]] = task.Status;
-        //     dataRow[Header[16]] = task.Observer;
-        //     dataRow[Header[17]] = task.FrameType;
-        //     dataRow[Header[18]] = task.Xbin;
-        //     dataRow[Header[19]] = task.Ybin;
-        //
-        //     DataTable.Rows.InsertAt(dataRow, 0);
-        //     // SaveTasksToXml();
-        // }
-
-        // public static void UpdateTaskInTable(ObservationTask task)
-        // {
-        //     foreach (DataGridViewRow row in DataGridViewTasker.Rows)
-        //     {
-        //         if (!string.Equals(row.Cells[0].Value.ToString(), task.TaskNumber.ToString())) continue;
-        //         row.Cells[0].Value = task.TaskNumber;
-        //         row.Cells[1].Value = task.RaDec;
-        //         row.Cells[2].Value = task.TimeAdd;
-        //         row.Cells[3].Value = task.TimeStart;
-        //         row.Cells[4].Value = task.TimeEnd;
-        //         row.Cells[5].Value = Math.Round(task.Duration, 2);
-        //         row.Cells[6].Value = task.Exp;
-        //         row.Cells[7].Value = task.DoneFrames;
-        //         row.Cells[8].Value = task.AllFrames;
-        //         row.Cells[9].Value = task.TimeLastExp;
-        //         row.Cells[10].Value = task.Filters.Contains("g");
-        //         row.Cells[11].Value = task.Filters.Contains("r");
-        //         row.Cells[12].Value = task.Filters.Contains("i");
-        //         row.Cells[13].Value = task.Object;
-        //         row.Cells[14].Value = task.ObjectType;
-        //         row.Cells[15].Value = task.Status;
-        //         row.Cells[16].Value = task.Observer;
-        //         row.Cells[17].Value = task.FrameType;
-        //         row.Cells[18].Value = task.Xbin;
-        //         row.Cells[19].Value = task.Ybin;
-        //     }
-        //     PaintTable();
-        //     // SaveTasksToXml();
-        // }
-
         public static void DeleteTaskByRowIndex(int rowIndex)
         {
             DataGridViewTasker.Rows.RemoveAt(rowIndex);
-            // SaveTasksToXml();
         }
-
-        // public static ObservationTask GetTaskByNumber(string taskNumber)
-        // {
-        //     var task = new ObservationTask();
-        //     foreach (DataGridViewRow row in DataGridViewTasker.Rows)
-        //     {
-        //         if (!string.Equals(row.Cells[0].Value.ToString(), taskNumber)) continue;
-        //         task.TaskNumber = Convert.ToInt32(row.Cells[0].Value);
-        //         if (!string.IsNullOrEmpty(row.Cells[1].Value.ToString()))
-        //         {
-        //             task.ComputeRaDec(row.Cells[1].Value.ToString());
-        //         }
-        //         task.TimeAdd = DateTime.Parse(row.Cells[2].Value.ToString());
-        //         task.TimeStart = DateTime.Parse(row.Cells[3].Value.ToString());
-        //         task.TimeEnd = DateTime.Parse(row.Cells[4].Value.ToString());
-        //         task.Duration = float.Parse(row.Cells[5].Value.ToString());
-        //         task.Exp = Convert.ToInt16(row.Cells[6].Value);
-        //         task.DoneFrames = Convert.ToInt16(row.Cells[7].Value);
-        //         task.AllFrames = Convert.ToInt16(row.Cells[8].Value);
-        //         task.TimeLastExp = (string.IsNullOrEmpty(row.Cells[9].Value.ToString()) ? 
-        //             new DateTime() : DateTime.Parse(row.Cells[9].Value.ToString()));
-        //         var f = "";
-        //         if ((bool)row.Cells[10].Value)
-        //         {
-        //             f += "g ";
-        //         }
-        //         if ((bool)row.Cells[11].Value)
-        //         {
-        //             f += "r ";
-        //         }
-        //         if ((bool)row.Cells[12].Value)
-        //         {
-        //             f += "i";
-        //         }
-        //         task.Filters = f;
-        //         task.Object = row.Cells[13].Value.ToString();
-        //         task.ObjectType = row.Cells[14].Value.ToString();
-        //         task.Status = Convert.ToInt16(row.Cells[15].Value);
-        //         task.Observer = row.Cells[16].Value.ToString();
-        //         task.FrameType = row.Cells[17].Value.ToString();
-        //         task.Xbin = Convert.ToInt16(row.Cells[18].Value);
-        //         task.Ybin = Convert.ToInt16(row.Cells[19].Value);
-        //         break;
-        //     }
-        //
-        //     return task;
-        // }
-        //
-        // public static void UpdateTaskFromTable(ObservationTask task)
-        // {
-        //     foreach (DataGridViewRow row in DataGridViewTasker.Rows)
-        //     {
-        //         if (!Equals(row.Cells[0].Value, task.TaskNumber)) continue;
-        //         task.TaskNumber = Convert.ToInt32(row.Cells[0].Value);
-        //         if (!string.IsNullOrEmpty(row.Cells[1].Value.ToString()))
-        //         {
-        //             task.ComputeRaDec(row.Cells[1].Value.ToString());
-        //         }
-        //         task.TimeAdd = DateTime.Parse(row.Cells[2].Value.ToString());
-        //         task.TimeStart = DateTime.Parse(row.Cells[3].Value.ToString());
-        //         task.TimeEnd = DateTime.Parse(row.Cells[4].Value.ToString());
-        //         task.Duration = float.Parse(row.Cells[5].Value.ToString());
-        //         task.Exp = Convert.ToInt16(row.Cells[6].Value);
-        //         task.DoneFrames = Convert.ToInt16(row.Cells[7].Value);
-        //         task.AllFrames = Convert.ToInt16(row.Cells[8].Value);
-        //         task.TimeLastExp = (string.IsNullOrEmpty(row.Cells[9].Value.ToString()) ? 
-        //             new DateTime() : DateTime.Parse(row.Cells[9].Value.ToString()));
-        //         
-        //         var f = "";
-        //         if ((bool)row.Cells[10].Value)
-        //         {
-        //             f += "g ";
-        //         }
-        //         if ((bool)row.Cells[11].Value)
-        //         {
-        //             f += "r ";
-        //         }
-        //         if ((bool)row.Cells[12].Value)
-        //         {
-        //             f += "i";
-        //         }
-        //         task.Filters = f;
-        //         task.Object = row.Cells[13].Value.ToString();
-        //         task.ObjectType = row.Cells[14].Value.ToString();
-        //         task.Status = Convert.ToInt16(row.Cells[15].Value);
-        //         task.Observer = row.Cells[16].Value.ToString();
-        //         task.FrameType = row.Cells[17].Value.ToString();
-        //         task.Xbin = Convert.ToInt16(row.Cells[18].Value);
-        //         task.Ybin = Convert.ToInt16(row.Cells[19].Value);
-        //         break;
-        //     }
-        // }
 
         public static ObservationTask GetTaskByRowIndex(int rowIndex)
         {
@@ -306,16 +119,21 @@ namespace RPCC.Tasks
             var f = "";
             if ((bool)row.Cells[10].Value)
             {
-                f += "g ";
+                f += $"{StringHolder.FilG} ";
+            }
+            if ((bool)row.Cells[22].Value)
+            {
+                f += $"{StringHolder.FilV} ";
             }
             if ((bool)row.Cells[11].Value)
             {
-                f += "r ";
+                f += $"{StringHolder.FilR} ";
             }
             if ((bool)row.Cells[12].Value)
             {
-                f += "i";
+                f += StringHolder.FilI;
             }
+            
             task.Filters = f;
             task.Object = DataGridViewTasker.Rows[rowIndex].Cells[13].Value.ToString();
             task.ObjectType = DataGridViewTasker.Rows[rowIndex].Cells[14].Value.ToString();
@@ -345,15 +163,19 @@ namespace RPCC.Tasks
             var f = "";
             if ((bool)row.ItemArray[10])
             {
-                f += "g ";
+                f += $"{StringHolder.FilG} ";
+            }
+            if ((bool)row.ItemArray[22])
+            {
+                f += $"{StringHolder.FilV} ";
             }
             if ((bool)row.ItemArray[11])
             {
-                f += "r ";
+                f += $"{StringHolder.FilR} ";
             }
             if ((bool)row.ItemArray[12])
             {
-                f += "i";
+                f += StringHolder.FilI;
             }
             task.Filters = f;
             task.Object = row.ItemArray[13].ToString();
